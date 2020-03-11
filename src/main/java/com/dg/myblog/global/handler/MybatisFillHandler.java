bacdae3190dfd8d4907c654bafe729013a593481
dantegarden <761043617@qq.com>
2020-03-11 17:30:30 +0800

    修改bug

--- src/main/java/com/dg/myblog/global/handler/MybatisFillHandler.java
+++ src/main/java/com/dg/myblog/global/handler/MybatisFillHandler.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * @description: mybatis-plus自动填充字段
@@ -18,34 +19,50 @@ import java.util.Map;
 @Slf4j
 public class MybatisFillHandler implements MetaObjectHandler {
 
-    private static final Map<String,Object> DEFAULT_INSERT_MAP = Maps.newLinkedHashMap();
-    private static final Map<String,Object> DEFAULT_UPDATE_MAP = Maps.newLinkedHashMap();
+    private static final Map<String,Supplier> DEFAULT_INSERT_MAP = Maps.newLinkedHashMap();
+    private static final Map<String,Supplier> DEFAULT_UPDATE_MAP = Maps.newLinkedHashMap();
 
     static {
-        DEFAULT_INSERT_MAP.put("createTime", new Date());
-        DEFAULT_INSERT_MAP.put("updateTime", new Date());
+        DEFAULT_INSERT_MAP.put("createTime", new Supplier() {
+            @Override
+            public Object get() {
+                return new Date();
+            }
+        });
+        DEFAULT_INSERT_MAP.put("updateTime", new Supplier() {
+            @Override
+            public Object get() {
+                return new Date();
+            }
+        });
 
-        DEFAULT_UPDATE_MAP.put("updateTime", new Date());
+        DEFAULT_UPDATE_MAP.put("updateTime", new Supplier() {
+            @Override
+            public Object get() {
+                return new Date();
+            }
+        });
     }
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        for (Map.Entry<String, Object> entry : DEFAULT_INSERT_MAP.entrySet()) {
+
+        for (Map.Entry<String, Supplier> entry : DEFAULT_INSERT_MAP.entrySet()) {
             String fieldName = entry.getKey();
             if(getFieldValByName(fieldName, metaObject) == null && metaObject.hasSetter(fieldName)){
                 log.info("自动填充属性{}", fieldName);
-                this.setInsertFieldValByName(fieldName, entry.getValue(), metaObject);
+                this.setInsertFieldValByName(fieldName, entry.getValue().get(), metaObject);
             }
         }
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        for (Map.Entry<String, Object> entry : DEFAULT_UPDATE_MAP.entrySet()) {
+        for (Map.Entry<String, Supplier> entry : DEFAULT_UPDATE_MAP.entrySet()) {
             String fieldName = entry.getKey();
             if(getFieldValByName(fieldName, metaObject) == null && metaObject.hasSetter(fieldName)){
                 log.info("自动更新属性{}", fieldName);
-                this.setInsertFieldValByName(fieldName, entry.getValue(), metaObject);
+                this.setInsertFieldValByName(fieldName, entry.getValue().get(), metaObject);
             }
         }
     }
